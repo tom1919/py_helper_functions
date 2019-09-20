cef7e86731fa571e58c8c41506c9ee4a92a5cb9f
Tommy Lang <35646257+tom1919@users.noreply.github.com>
2019-09-19 23:50:32 -0400

    Create helper_functions.py

---
+++ helper_functions.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+lots of documentation
+"""
+
+#%%
+def get_path(name):
+    '''
+    Returns string for file path to directory
+    '''
+    if name == 'team':
+        ret_path = 'C:/Users/'
+    elif name == 'tom':
+        ret_path = 'C:/Users/Dropbox/7.projects'
+    else:
+        raise ValueError("Not a valid argument. Use 'tom' or 'team'.")
+    return ret_path
+        
+if __name__ == "__main__":
+    foo = get_path('tof')
+    hf.get_path??
+    hf??
+
+#%% 
+
+## db connection
+
+
+# =============================================================================
+# put it one of these paths
+# import sys
+# sys.path
+# =============================================================================
+
+
+
